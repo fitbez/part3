7d3c66275e2bec66899698c808bcf102b943590f
fitsum belayneh <fitsu_alt@yahoo.com>
2019-12-19 10:03:28 -0500

    styling contact list

--- client/src/components/Persons.js
+++ client/src/components/Persons.js
@@ -5,18 +5,24 @@ const Persons = ({ person, handleDelete, persons }) => {
     <li>
       <i className="fas fa-user 3x"></i>
       {person.name}
-      <i className="fas fa-phone 2x"></i>
-      {person.number}
-      <i
-        onClick={() => {
-          return window.confirm(`Delete ${person.name}?`)
-            ? handleDelete(person.id)
-            : null;
-        }}
-        className="far fa-trash-alt 3x"
-      >
+      <div className="phone">
         {" "}
-      </i>
+        <i className="fas fa-mobile"></i>
+        {person.number}
+      </div>
+
+      <div className="delete">
+        <i
+          onClick={() => {
+            return window.confirm(`Delete ${person.name}?`)
+              ? handleDelete(person.id)
+              : null;
+          }}
+          className="far fa-trash-alt 3x"
+        >
+          {" "}
+        </i>
+      </div>
     </li>
   );
 };
